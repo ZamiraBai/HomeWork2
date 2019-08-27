39a2a7d2a710c3fe063cf67c2e9d3ed786b7d27e
ZamiraBai <baimurzaeva@hotmail.com>
2019-08-27 20:04:28 +0600

    Изменен

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
         String name = "Ilana";
-        System.out.println(name);
-        int age = 23;
-        System.out.println(age);
+        int age = 22;
         int temp = 28;
-        if (temp < 30 || temp > -20 && age > 20 || age < 45) ;
-        if (temp < 0 || temp > 28 && age < 20) ;
-        if (temp < -10 || temp > 25 && age > 45) ;
-        else {
+        if ((temp > 30 || temp < -20) || (age > 20 || age < 45)) {
             System.out.println("Не выходим гулять!");
+        } else {
+            System.out.println("Идем в гости к другу!");
+
+        }
+        if ((temp > 0 || temp < 28) && (age < 20)) {
+            System.out.println("Не выходим гулять!");
+        } else {
+            System.out.println("Идем в гости к другу!");
+
+        }
+        if ((temp > -10 || temp < 25) && (age < 45)) {
+            System.out.println("Не выходим гулять!");
+
+        } else {
             System.out.println("Идем в гости к другу!");
         }
+
     }
 }
